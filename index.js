be97e7e5330295d497be0f6a5d7a6cb5769c84e8
SimranDev <simran.develop@gmail.com>
2021-05-29 20:17:22 +0530

    8 problems solved

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*=============================================================
+/*1.=============================================================
 ❓  Print the sum of the array's elements as a single integer.
 */
 // 📢 expected output: 40
@@ -6,10 +6,21 @@ var arr = [4, 3, 5, 6, 8, 9, 5];
 var simpleArrSum = function (arr) {
     var num = 0;
     arr.map(function (val) { return (num += val); });
-    console.log(num);
+    console.log("Simple Array Sum: " + num);
 };
 simpleArrSum(arr);
-/*===============================================================
+/*2.===============================================================
+❓  A Very Big Sum
+Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements
+*/
+var arr1 = [
+    1000000001, 1000000002, 1000000003, 1000000004, 1000000005,
+];
+var aVeryBigSum = function (arr) {
+    return arr.reduce(function (previousVal, currentVal) { return previousVal + currentVal; });
+};
+console.log("A very Bigg Num: " + aVeryBigSum(arr1));
+/*3.===============================================================
 ❓  Compare the Triplets
 The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
 If a[i] > b[i], then Alice is awarded 1 point.
@@ -33,12 +44,12 @@ var compareTriplets = function (arrA, arrB) {
     }
     return [aScores, bScrores];
 };
-console.log(compareTriplets(a, b));
-/*===============================================================
+console.log("Compare Triplets: " + compareTriplets(a, b));
+/*4.===============================================================
 ❓   Min Max Sum
 Print two space-separated integers on one line: the minimum sum and the maximum sum of 4 of 5 elements */
 // 📢 expected output: 16 24
-var arr1 = [1, 3, 5, 7, 9];
+var arr2 = [1, 3, 5, 7, 9];
 var minMax = function (arr) {
     var minTotal, maxTotal;
     minTotal = arr
@@ -49,10 +60,10 @@ var minMax = function (arr) {
         .sort()
         .filter(function (_, i) { return i !== 0; })
         .reduce(function (a, b) { return a + b; });
-    console.log(minTotal + " " + maxTotal);
+    console.log("Min Max Sum: " + minTotal + " " + maxTotal);
 };
-minMax(arr1);
-/*===============================================================
+minMax(arr2);
+/*5.===============================================================
 ❓  Birdthday Cake Candles
   You are in charge of the cake for a child's birthday.
  You have decided the cake will have one candle for each year of their total age.
@@ -82,4 +93,48 @@ function birthdayCakeCandles(candles) {
     }
     return count;
 }
-console.log(birthdayCakeCandles(candles));
+console.log("Birthday Cake Candles: " + birthdayCakeCandles(candles));
+//6.===============================================================
+/* 🌎  LINK: https://www.hackerrank.com/challenges/diagonal-difference/problem */
+var arr2D = [
+    [8, 2, 3],
+    [1, 7, 3],
+    [3, 6, 1],
+];
+var diagonalDifference = function (arr) {
+    var diagonal1 = 0;
+    var diagonal2 = 0;
+    for (var i in arr) {
+        diagonal1 += arr[i][i];
+        diagonal2 += arr.reverse()[i][i];
+    }
+    return Math.abs(diagonal1 - diagonal2);
+};
+console.log("Diagonal Difference " + diagonalDifference(arr2D));
+//7.===============================================================
+/* 🌎  LINK: https://www.hackerrank.com/challenges/plus-minus/problem */
+var arr3 = [-4, 3, -9, 0, 4, 1];
+var plusMinus = function (arr) {
+    var positiveNum = 0, negativeNum = 0, zeroNum = 0;
+    for (var i in arr) {
+        if (arr[i] > 0) {
+            positiveNum++;
+        }
+        else if (arr[i] < 0) {
+            negativeNum++;
+        }
+        else
+            zeroNum++;
+    }
+    var length = arr.length;
+    console.log(positiveNum / length, negativeNum / length, zeroNum / length);
+};
+plusMinus(arr3);
+//8.===============================================================
+// 🌎 https://www.hackerrank.com/challenges/staircase/problem
+var staircase = function (num) {
+    for (var i = 0; i < num; i++) {
+        console.log(" ".repeat(num - (i + 1)) + "#".repeat(i + 1));
+    }
+};
+staircase(10);
--- index.ts
+++ index.ts
@@ -1,16 +1,30 @@
-/*=============================================================
+/*1.=============================================================
 ❓  Print the sum of the array's elements as a single integer.
 */
 // 📢 expected output: 40
-let arr: number[] = [4, 3, 5, 6, 8, 9, 5];
+const arr: number[] = [4, 3, 5, 6, 8, 9, 5];
+
 const simpleArrSum = (arr: number[]): void => {
   let num: number = 0;
   arr.map((val) => (num += val));
-  console.log(num);
+  console.log(`Simple Array Sum: ${num}`);
 };
 simpleArrSum(arr);
 
-/*===============================================================
+/*2.===============================================================
+❓  A Very Big Sum 
+Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements
+*/
+const arr1: number[] = [
+  1000000001, 1000000002, 1000000003, 1000000004, 1000000005,
+];
+
+const aVeryBigSum = (arr: number[]): number => {
+  return arr.reduce((previousVal, currentVal) => previousVal + currentVal);
+};
+console.log(`A very Bigg Num: ${aVeryBigSum(arr1)}`);
+
+/*3.===============================================================
 ❓  Compare the Triplets
 The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
 If a[i] > b[i], then Alice is awarded 1 point.
@@ -31,13 +45,13 @@ const compareTriplets = (arrA: number[], arrB: number[]): number[] => {
   }
   return [aScores, bScrores];
 };
-console.log(compareTriplets(a, b));
+console.log(`Compare Triplets: ${compareTriplets(a, b)}`);
 
-/*===============================================================
+/*4.===============================================================
 ❓   Min Max Sum 
 Print two space-separated integers on one line: the minimum sum and the maximum sum of 4 of 5 elements */
 // 📢 expected output: 16 24
-let arr1: number[] = [1, 3, 5, 7, 9];
+const arr2: number[] = [1, 3, 5, 7, 9];
 const minMax = (arr: number[]): void => {
   let minTotal: number, maxTotal: number;
   minTotal = arr
@@ -49,11 +63,11 @@ const minMax = (arr: number[]): void => {
     .filter((_, i) => i !== 0)
     .reduce((a, b) => a + b);
 
-  console.log(`${minTotal} ${maxTotal}`);
+  console.log(`Min Max Sum: ${minTotal} ${maxTotal}`);
 };
-minMax(arr1);
+minMax(arr2);
 
-/*===============================================================
+/*5.===============================================================
 ❓  Birdthday Cake Candles
   You are in charge of the cake for a child's birthday.
  You have decided the cake will have one candle for each year of their total age.
@@ -83,5 +97,57 @@ function birthdayCakeCandles(candles: number[]): number {
   }
   return count;
 }
+console.log(`Birthday Cake Candles: ${birthdayCakeCandles(candles)}`);
 
-console.log(birthdayCakeCandles(candles));
+//6.===============================================================
+/* 🌎  LINK: https://www.hackerrank.com/challenges/diagonal-difference/problem */
+const arr2D: number[][] = [
+  [8, 2, 3],
+  [1, 7, 3],
+  [3, 6, 1],
+];
+
+const diagonalDifference = (arr: number[][]) => {
+  let diagonal1: number = 0;
+  let diagonal2: number = 0;
+
+  for (const i in arr) {
+    diagonal1 += arr[i][i];
+    diagonal2 += arr.reverse()[i][i];
+  }
+  return Math.abs(diagonal1 - diagonal2);
+};
+
+console.log(`Diagonal Difference ${diagonalDifference(arr2D)}`);
+
+//7.===============================================================
+/* 🌎  LINK: https://www.hackerrank.com/challenges/plus-minus/problem */
+const arr3: number[] = [-4, 3, -9, 0, 4, 1];
+
+const plusMinus = (arr: number[]): void => {
+  let positiveNum: number = 0,
+    negativeNum: number = 0,
+    zeroNum: number = 0;
+
+  for (const i in arr) {
+    if (arr[i] > 0) {
+      positiveNum++;
+    } else if (arr[i] < 0) {
+      negativeNum++;
+    } else zeroNum++;
+  }
+
+  const length: number = arr.length;
+  console.log(positiveNum / length, negativeNum / length, zeroNum / length);
+};
+
+plusMinus(arr3);
+
+//8.===============================================================
+// 🌎 https://www.hackerrank.com/challenges/staircase/problem
+const staircase = (num: number): void => {
+  for (let i = 0; i < num; i++) {
+    console.log(" ".repeat(num - (i + 1)) + "#".repeat(i + 1));
+  }
+};
+staircase(10);
